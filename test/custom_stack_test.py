b158e2a324b68357a9588cf5e51ab4078c93e85a
Jonatas Mathias <jonatasmathiasdallo@gmail.com>
2025-05-08 19:48:28 -0300

    Commit da primeira parte da prática fazendo a cobertura de 79% alcançar 100%

---
+++ test/custom_stack_test.py
@@ -0,0 +1,36 @@
+import pytest
+from src.custom_stack_class import *
+
+def test_push_one_elementinstack():
+    element_value = 5.0
+    cut = CustomStack(5)
+    cut.push(element_value)
+    assert not cut.is_empty()
+    assert cut.top() == element_value
+    assert cut.size() == 1
+
+def test_push_until_full_raises_exception():
+    stack = CustomStack(2)
+    stack.push(1)
+    stack.push(2)
+    with pytest.raises(StackFullException):
+        stack.push(3)
+
+def test_pop_from_empty_stack_raises_exception():
+    stack = CustomStack(2)
+    with pytest.raises(StackEmptyException):
+        stack.pop()
+
+def test_top_from_empty_stack_raises_exception():
+    stack = CustomStack(2)
+    with pytest.raises(StackEmptyException):
+        stack.top()
+
+def test_push_and_pop_sequence():
+    stack = CustomStack(3)
+    stack.push('a')
+    stack.push('b')
+    assert stack.top() == 'b'
+    assert stack.pop() == 'b'
+    assert stack.top() == 'a'
+    assert stack.size() == 1
